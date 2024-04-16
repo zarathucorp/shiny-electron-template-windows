d87cef4fb7211a16bb9cecdd0e9fdcf39e99afed
Jinhwan Kim <hwanistic@gmail.com>
2024-04-16 13:31:14 +0900

    Update get-r-win.sh
    update R version from 4.3.1 to 4.3.2

--- get-r-win.sh
+++ get-r-win.sh
@@ -1,9 +1,9 @@
 # Copyright (c) 2018 Dirk Schumacher, Noam Ross, Rich FitzJohn
-# 2023 Jinhwan Kim
+# copyright (c) 2023 Jinhwan Kim
 # Download and extract the Windows binary install
 mkdir r-win
 
-curl -o r-win/latest_r.exe https://cloud.r-project.org/bin/windows/base/old/4.3.1/R-4.3.1-win.exe
+curl -o r-win/latest_r.exe https://cloud.r-project.org/bin/windows/base/old/4.3.2/R-4.3.2-win.exe
 
 cd r-win
 innoextract -e latest_r.exe
